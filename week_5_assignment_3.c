57ec84531ae570e86f8aa317fc76cf3808f81d78
rohan37-pro <rohan2002maji@gmail.com>
2023-08-28 21:52:13 -0700

    week 5 assignment 3 is updated

--- week_5_assignment_3.c
+++ week_5_assignment_3.c
@@ -16,19 +16,21 @@ Use the printf statements as below
 printf("%d is a number that can be expressed as power of 2.",N);
 printf("%d cannot be expressed as power of 2.",N);
 */
-int n=N, flag=1;
-while (n>1){
-  if (n%2!=0){
-    flag=0;
-    break;
-  }
-  else{
-  	n/=2;
-  }
-}
-if (flag==1)
-	printf("%d is a number that can be expressed as power of 2.",N);
-else
-  printf("%d cannot be expressed as power of 2.",N);
-  
+    int n=N, flag=1;
+    while (n>1){
+        if (n%2!=0){
+            flag=0;
+            break;
+        }
+        else{
+            n/=2;
+        }
+    }
+    
+    if (flag==1)
+        printf("%d is a number that can be expressed as power of 2.",N);
+    else
+        printf("%d cannot be expressed as power of 2.",N);
+    
+    return 0;
 }
