32f75ac5ccf37aa96aae52752f66c3fc53a0bb2f
rohan37-pro <rohan2002maji@gmail.com>
2023-10-04 18:26:15 -0700

    small changes in assingment 3

--- week_11_assignment_3.c
+++ week_11_assignment_3.c
@@ -7,17 +7,17 @@ Write a C program to solve the following differential equation using Runge-Kutta
 
 
 #include<stdio.h>
+
 float func(float x,float y);
-int main()
-{
+
+int main(){
     float m1,m2,m3,m4,m,h=0.3;
     float x0 = 0.3, y0 = 5, xn;
     scanf("%f",&xn); //xn will be taken from test cases
 
 
-//Use the printf statement as: printf("y=%f",y);
-while(x0<xn)
-    {
+    //Use the printf statement as: printf("y=%f",y);
+    while(x0<xn){
         m1=func(x0,y0);
         m2=func((x0+h/2.0),(y0+m1*h/2));
         m3=func((x0+h/2.0),(y0+m2*h/2));
@@ -27,10 +27,13 @@ while(x0<xn)
         x0=x0+h;
     }
     printf("y=%f",y0);  // Final output
+
+
     return 0;
 }
-float func(float x,float y)
-{
+
+
+float func(float x,float y){
     float m;
     m=(x*(x+1)-3*y*y*y)/10;
     return m;
