7e88aa211b93f3bc251ea1e74aacfb6085d5525d
rohan37-pro <rohan2002maji@gmail.com>
2023-10-12 22:57:14 -0700

    assignment 4 is formated

--- week_11_assignment_4.c
+++ week_11_assignment_4.c
@@ -7,38 +7,35 @@ Note that you have to use recursion.
 
 
 #include <stdio.h>
+
 int checkPrime(int, int); //Function to check prime or not 
 
-int main()
-{
+int main(){
     int num, check;
     scanf("%d", &num); //The number is taken from test case data
     check = checkPrime(num, num/2);
-    if (check == 1)
-    {
+
+    if (check == 1){
         printf("%d is a prime number\n", num);
     }
-    else
-    {
+    else{
         printf("%d is not a prime number\n", num);
     }
+
     return 0;
 }
-int checkPrime(int num, int i)
-{
-    if (i == 1)
-    {
+
+
+int checkPrime(int num, int i){
+    if (i == 1){
         return 1;
     }
-    else
-    {
-       if (num % i == 0)
-       {
-         return 0;
-       }
-       else
-       {
-         return checkPrime(num, i - 1);
-       }
+    else{
+        if (num % i == 0){
+            return 0;
+        }
+        else{
+            return checkPrime(num, i - 1);
+        }
     }
 }
