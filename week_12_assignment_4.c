7df652e54da81836efcc0bd3d60b84d0378bc34f
rohan37-pro <rohan2002maji@gmail.com>
2023-10-16 10:55:32 -0700

    assignment 4  is on going..

---
+++ week_12_assignment_4.c
@@ -0,0 +1,31 @@
+// problem statement
+/*
+Write a C program to find the sum of two 1D integer arrays ‘A’  and ‘B’ of same size and store the result in another array ‘C’, where the size of the array and the elements of the array are taken as input.
+In the Test case the input is given as follows
+
+5
+10
+20
+30
+40
+50
+1
+2
+3
+4
+5
+
+So the output will be displayed as
+
+Result is
+
+11
+22
+33
+44
+55
+
+Write the program accordingly. Use dynamic memory allocation. 
+*/
+
+
