d0c9bb6a8bf5f3e9093e88f96e5f753c4249f747
rohan37-pro <rohan2002maji@gmail.com>
2023-10-16 10:59:47 -0700

    assignment 4 is done now

--- week_12_assignment_4.c
+++ week_12_assignment_4.c
@@ -29,3 +29,42 @@ Write the program accordingly. Use dynamic memory allocation.
 */
 
 
+#include<stdio.h>
+#include<stdlib.h>
+
+void main()
+{
+ int i,n;
+ 
+//The number of elements in each array is taken from test case data 
+
+ scanf("%d", &n);
+int *a,*b,*c;
+ a = (int *) malloc(n*sizeof(int));
+ b = (int *) malloc(n*sizeof(int));
+ c = (int *) malloc(n*sizeof(int));
+
+// Input Elements of First List;
+ for(i=0;i<n;i++)
+ {
+  scanf("%d",a+i);
+ }
+
+ //Input Elements of Second List;
+ for(i=0;i<n;i++)
+ {
+  scanf("%d",b+i);
+ }
+
+ for(i=0;i<n;i++)
+ {
+  *(c+i) = *(a+i) + *(b+i);
+ }
+printf("Result is\n");
+
+ for(i=0; i<n; i++)
+ {
+  printf("%d\n",*(c+i));
+ }
+
+}
